388e5b3d7bbd70d78f125bb21e40382b193f2091
Johan <johasel@stud.ntnu.no>
2019-03-29 12:14:53 +0100

    test 1

--- config.php
+++ config.php
@@ -4,15 +4,10 @@ $dbport = "26257";
 $db = "bf";
 $dbuser = "bfuser";
 $dbpassw = '';
-$webhost = '';
+$webhost = '10.212.136.246';
 $weburl = 'http://' . $webhost ;
-$frontpage_limit = 100000;
-if ( getenv("BF_FRONTPAGE_LIMIT") ){
-   $frontpage_limit = getenv("BF_FRONTPAGE_LIMIT");
-}
-if ( getenv("BF_MEMCACHE_SERVER")){
-   $memcache_enabled_pictures = 1;
-   $memcache_server = getenv("BF_MEMCACHE_SERVER");
-   $memcache_enabled = 1;
-}
+$frontpage_limit = 500;
+$memcache_enabled_pictures = 1;
+$memcache_server = getenv("MEMCACHE_SERVER");
+$memcache_enabled = 1;
 ?>
