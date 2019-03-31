ae849dc7bebcfce3c53979d694c96599dc7cb0e9
Johan <johasel@stud.ntnu.no>
2019-03-31 22:10:37 +0200

    asd

--- deploystack.sh
+++ deploystack.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-
-CREATETABLES='CREATE DATABASE bf;
-CREATE USER bfuser;
-GRANT ALL ON DATABASE bf TO bfuser;
-USE bf;
-CREATE table users ( userID INT PRIMARY KEY DEFAULT unique_rowid(), name STRING(50), picture STRING(300), status STRING(10), posts INT, comments INT, lastPostDate TIMESTAMP DEFAULT NOW(), createDate TIMESTAMP DEFAULT NOW());
-CREATE table posts ( postID INT PRIMARY KEY DEFAULT unique_rowid(), userID INT, text STRING(300), name STRING(150), postDate TIMESTAMP DEFAULT NOW());
-CREATE table comments ( commentID INT PRIMARY KEY DEFAULT unique_rowid(), postID INT, userID INT, text STRING(300),  postDate TIMESTAMP DEFAULT NOW());'
-
 sudo docker stack deploy -c=/home/ubuntu/bookface2.0/docker-compose2.0.yml bf;
 sleep 5
 echo "Start"
